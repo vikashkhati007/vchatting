b06061bbb6478695ce6c503f5778307663b77f57
vikashkhati007 <vikashkhati007@gmail.com>
2024-01-10 08:41:27 +0530

    middleware

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -6,6 +6,7 @@ import { createUser } from "@/lib/actions/user.actions";
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  console.log(WEBHOOK_SECRET);
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
@@ -52,7 +53,7 @@ export async function POST(req: Request) {
   // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
-
+  console.log(evt.data);
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
